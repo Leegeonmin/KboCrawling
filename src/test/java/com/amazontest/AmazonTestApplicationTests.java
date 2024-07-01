22529b17e49603f1dc0529c0b91d20dea9b801e9
Leegeonmin <kmlee97s@gmail.com>
2024-07-02 02:47:48 +0900

    Initial commit

---
+++ src/test/java/com/amazontest/AmazonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
