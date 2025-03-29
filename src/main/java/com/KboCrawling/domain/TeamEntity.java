f08362bbd4111b108843a84b3d6015e321bb55cc
Leegeonmin <kmlee97s@gmail.com>
2025-03-29 21:54:35 +0900

    feaet : 팀 entity 추가

---
+++ src/main/java/com/KboCrawling/domain/TeamEntity.java
@@ -0,0 +1,37 @@
+package com.KboCrawling.domain;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "team")
+@NoArgsConstructor
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class TeamEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stadium_id", nullable = false)
+    private StadiumEntity stadium;
+
+    @NotBlank
+    @Column(length = 20)
+    private String name;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+    @CreatedDate
+    private LocalDateTime createdDate;
+}
